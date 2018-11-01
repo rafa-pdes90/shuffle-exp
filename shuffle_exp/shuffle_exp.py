18e021ab0f558554030441c976fdd39e44c03a55
rafa-pdes90 <rafa-pdes90@users.noreply.github.com>
2018-11-01 19:31:18 -0300

    print improvements and also added balanced shuffle

--- shuffle_exp/shuffle_exp.py
+++ shuffle_exp/shuffle_exp.py
@@ -1,5 +1,6 @@
 from fisher_yates_shuffle import fisher_yates_shuffle as fisher_yates
 from sattolo_shuffle import sattolo_shuffle as sattolo
+from balanced_shuffle import balanced_shuffle as balanced
 from spotify_shuffle import spotify_shuffle as spotify
 
 class Song:
@@ -8,29 +9,38 @@ class Song:
     self.artist = artist
     self.album = album
 
+  def __str__(self):
+    return "{0.name} {0.artist} {0.album}".format(self)
 
-x = [Song(1, "A", "a1"), Song(2, "A", "a2"), Song(3, "B", "b1"), Song(4, "C", "c1"),
-     Song(5, "B", "b1"), Song(6, "A", "a1"), Song(7, "D", "d1"), Song(8, "A", "a1"),
-     Song(9, "D", "d2")]
+if __name__=='__main__':
+  x = [Song(1, "A", "a1"), Song(2, "A", "a2"), Song(3, "B", "b1"), Song(4, "C", "c1"),
+      Song(5, "B", "b1"), Song(6, "A", "a1"), Song(7, "D", "d1"), Song(8, "A", "a1"),
+      Song(9, "D", "d2")]
 
-print ("Original:")
-for song in x:
-  print(song.name, song.artist, song.album)
+  print ("Original:")
+  for song in x:
+    print(song)
 
-print ("\r\nFisher-Yates Shuffle:")
-test = list(x)
-fisher_yates.shuffle(test)
-for song in test:
-  print(song.name, song.artist, song.album)
+  print ("\r\nFisher-Yates Shuffle:")
+  test = list(x)
+  fisher_yates.shuffle(test)
+  for song in test:
+    print(song)
 
-print ("\r\nSattolo Shuffle:")
-test = list(x)
-sattolo.shuffle(test)
-for song in test:
-  print(song.name, song.artist, song.album)
+  print ("\r\nSattolo Shuffle:")
+  test = list(x)
+  sattolo.shuffle(test)
+  for song in test:
+    print(song)
 
-print ("\r\nSpotify Shuffle:")
-test = list(x)
-spotify.shuffle(test, ["artist", "album"])
-for song in test:
-  print(song.name, song.artist, song.album)
+  print ("\r\nBalanced Shuffle:")
+  test = list(x)
+  balanced.shuffle(test, ["artist", "album"])
+  for song in test:
+    print(song)
+
+  print ("\r\nSpotify Shuffle:")
+  test = list(x)
+  spotify.shuffle(test, ["artist", "album"])
+  for song in test:
+    print(song)
