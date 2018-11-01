1e7f62f1bc8222ef3da5e0f484e1080c061f06f5
rafa-pdes90 <rafa-pdes90@users.noreply.github.com>
2018-11-01 16:28:32 -0300

    fixed random offset and rearranged the steps a bit

--- shuffle_exp/spotify_shuffle/spotify_shuffle.py
+++ shuffle_exp/spotify_shuffle/spotify_shuffle.py
@@ -11,22 +11,21 @@ def shuffle(x, y):
     songs_by_y[y_attrib].append(song)
 
   for songs in songs_by_y.values():
-    song_count = len(songs)
-    appear_base = 100/song_count
-    appear_lim = appear_base - 100/(song_count+1)
-    appear_max, appear_min = appear_base + appear_lim, appear_base - appear_lim
-    
-    offset_lim = 100.0 - (appear_max * (song_count - 1))
-    offset = random.uniform(0, offset_lim)
-
     if len(y) > 1:
       shuffle(songs, y[1:])
 
     random.shuffle(songs)
 
+    song_count = len(songs)
+    appear_base = 100.0/song_count
+    appear_lim = appear_base - 100.0/(song_count+1)
+    appear_max, appear_min = min(100.0, appear_base + appear_lim), max(0.0, appear_base - appear_lim)
+    
+    offset = random.uniform(0.0, 100.0)
+
     songs_pos[songs[0]] = offset
     
     for i in range (1, song_count):
-      songs_pos[songs[i]] = songs_pos[songs[i-1]] + random.uniform(appear_min, appear_max)
+      songs_pos[songs[i]] = (songs_pos[songs[i-1]] + random.uniform(appear_min, appear_max)) % 100
 
   x.sort(key=lambda i: songs_pos[i])
