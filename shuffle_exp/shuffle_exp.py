60948d8721b8d8a4fc308758d3ab8b0e062c3cdf
rafa-pdes90 <rafa-pdes90@users.noreply.github.com>
2018-11-03 19:01:17 -0300

    weight added to Song class

--- shuffle_exp/shuffle_exp.py
+++ shuffle_exp/shuffle_exp.py
@@ -5,18 +5,19 @@ from spotify_shuffle import spotify_shuffle as spotify
 from my_shuffle import my_shuffle as my
 
 class Song:
-  def __init__(self, name, artist, album):
+  def __init__(self, name, artist, album, weight):
     self.name = name
     self.artist = artist
     self.album = album
+    self.weight = weight
 
   def __str__(self):
     return "{0.name} {0.artist} {0.album}".format(self)
 
 if __name__=='__main__':
-  x = [Song(1, "A", "a1"), Song(2, "A", "a2"), Song(3, "B", "b1"), Song(4, "C", "c1"),
-      Song(5, "B", "b1"), Song(6, "A", "a1"), Song(7, "D", "d1"), Song(8, "A", "a1"),
-      Song(9, "D", "d2")]
+  x = [Song(1, "A", "a1", 3), Song(2, "A", "a2", 7), Song(3, "B", "b1", 1), Song(4, "C", "c1", 2),
+      Song(5, "B", "b1", 5), Song(6, "A", "a1", 9), Song(7, "D", "d1", 6), Song(8, "A", "a1", 4),
+      Song(9, "D", "d2", 8)]
 
   print ("Original:")
   for song in x:
@@ -50,8 +51,8 @@ if __name__=='__main__':
   print ("\r\nMy Shuffle:")
   for i in range(10000):
     test = list(x)
-    my.shuffle(test, ["artist"], [3, 7, 1, 2, 5, 9, 6, 4, 8])
-    index = test.index(x[5])
+    my.shuffle(test, ["artist", "album"], "weight")
+    index = test.index(x[1])
     hap[index+1] += 1
   sum = 0
   for v in hap.values():
