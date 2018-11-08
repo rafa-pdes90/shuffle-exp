30091e904da598025c16d9b0ca9197bd2ee230b6
rafa-pdes90 <rafa-pdes90@users.noreply.github.com>
2018-11-08 20:17:56 -0300

    fully implemented

--- shuffle_exp/my_shuffle/my_shuffle.py
+++ shuffle_exp/my_shuffle/my_shuffle.py
@@ -1,29 +1,18 @@
 import random
-import numpy
-import statistics
 
 def shuffle(x, y, w):
   def pos(song, weight, w_step):
     mode = 100.0 - (getattr(song, weight) - random.random())*w_step
-    return numpy.random.triangular(0.0, mode, 100.0)
+    return random.triangular(0.0, 100.0, mode)
   
-  w_min=float("inf")
-  w_max=0
-  
-  for i in range(len(x)):
-    weight = getattr(x[i], w)
-    w_min = min(w_min, weight)
-    w_max = max(w_max, weight)
-
-  w_step = 100.0 / (w_max - w_min + 1)
+  w_max = max([getattr(x[i], w) for i in range(len(x))])
+  w_step = 100.0 / w_max
   x.sort(key=lambda i: pos(i, w, w_step))
   weighted_shuffle(x, y, w)
 
 def weighted_shuffle(x, y, w):
   songs_by_y = {}
-  songs_pos = {}
   song_count = len(x)
-  g_step = 1.0/song_count
   
   for i in range(song_count):
     y_attrib = getattr(x[i], y[0])
@@ -31,27 +20,41 @@ def weighted_shuffle(x, y, w):
       songs_by_y[y_attrib] = []
     songs_by_y[y_attrib].append((x[i], i))
 
-  for songs in songs_by_y.values():
-    if len(y) > 1:
-      weighted_shuffle(songs, y[1:], w)
-    
-    group_count = len(songs)
+  songs_list = songs_by_y.values()
 
-    if group_count == 1:
-      songs_pos[songs[0][0]] = songs[0][1]/(song_count-1)
-    else:
-      first = songs[0][1]/(song_count-1)
-      last = songs[-1][1]/(song_count-1)
-      min_gap = group_count * g_step
+  if len(songs_list) > 1:
+    songs_pos = {}
+    g_step = 1.0 / song_count
 
-      if (last - first) < min_gap:
-        max_gap = (group_count+1) * g_step
-        inc = random.uniform(min_gap, max_gap)/2
-        first = max(0.0, first - inc)
-        last = min(1.0, last + inc)
+    for songs in songs_list:
+      if len(y) > 1:
+        weighted_shuffle(songs, y[1:], w)
+      
+      i = 0
+      j = len(songs) - 1
+      lim_min = 0.0
+      lim_max = 1.0
 
-      songs_pos[songs[0][0]] = first
-      songs_pos[songs[-1][0]] = last
+      while i < j:
+        first = songs[i][1]/(song_count-1)
+        last = songs[j][1]/(song_count-1)
+        sub_count = j-i + 1
+        min_gap = sub_count * g_step
+        
+        if (last - first) < min_gap:
+          max_gap = (sub_count+1) * g_step
+          inc = random.uniform(min_gap, max_gap)/2
+          first = max(lim_min, first - inc)
+          last = min(lim_max, last + inc)
 
+        lim_min = songs_pos[songs[i][0]] = first
+        lim_max = songs_pos[songs[j][0]] = last
+        i += 1
+        j -= 1
+      
+      if i == j:
+        songs_pos[songs[i][0]] = songs[i][1]/(song_count-1)
 
-  x.sort(key=lambda i: songs_pos[i])
+    x.sort(key=lambda i: songs_pos[i])
+  elif len(y) > 1:
+    weighted_shuffle(x, y[1:], w)
