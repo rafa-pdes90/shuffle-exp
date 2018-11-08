8caf25dd87322c5718a1f65b1aee22c4800c529c
rafa-pdes90 <rafa-pdes90@users.noreply.github.com>
2018-11-08 20:35:02 -0300

    cleaned

--- shuffle_exp/my_shuffle/sorteddict.py
+++
@@ -1,6 +0,0 @@
-from sortedcontainers import SortedDict as OriginalSortedDict
-
-class SortedDict(OriginalSortedDict):
-  def __setitem__(self, key, value):
-    super(SortedDict, self).__setitem__(key, value)
-    return self.index(key)
