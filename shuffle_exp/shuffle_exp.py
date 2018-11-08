8d2f0f891b305655512bf840ea20051ccf571db5
rafa-pdes90 <rafa-pdes90@users.noreply.github.com>
2018-11-08 20:20:42 -0300

    basic tests for all

--- shuffle_exp/shuffle_exp.py
+++ shuffle_exp/shuffle_exp.py
@@ -17,7 +17,6 @@ class Song:
     return "{0.name} {0.artist} {0.album} {0.weight}".format(self)
 
 if __name__=='__main__':
-  '''
   x = [Song(1, "A", "a1", 3), Song(2, "A", "a2", 7), Song(3, "B", "b1", 1), Song(4, "C", "c1", 2),
       Song(5, "B", "b1", 5), Song(6, "A", "a1", 9), Song(7, "D", "d1", 6), Song(8, "A", "a1", 4),
       Song(9, "D", "d2", 8)]
@@ -50,35 +49,25 @@ if __name__=='__main__':
   for song in test:
     print(song)
 
-  hap = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
   print ("\r\nMy Shuffle:")
-  for i in range(1):
-    test = list(x)
-    my.shuffle(test, ["artist", "album"], "weight")
-    index = test.index(x[1])
-    hap[index+1] += 1
-  sum = 0
-  for v in hap.values():
-    sum += v
-  for i in range(1, 10):
-    print (i, ": ", (hap[i]/sum)*100, "%")
-  '''
-  u = [Song(1, "A", "a1", 9), Song(2, "B", "b1", 8), Song(3, "C", "c1", 7), Song(4, "D", "d1", 6),
-      Song(5, "E", "e1", 5), Song(6, "F", "f1", 4), Song(7, "G", "g1", 3), Song(8, "H", "h1", 2),
-      Song(9, "I", "i1", 1)]
-  
-  hap = {i:[] for i in range(len(u))}
-  print()
-  total = 10000
+  hap = {i:[] for i in range(len(x))}
+  modo = 0
+  total = 1 * (10000**modo)
   for i in range(total):
-    test = list(u)
+    test = list(x)
     my.shuffle(test, ["artist"], "weight")
-    for j in range(len(u)):
-      index = test.index(u[j]) + 1
-      hap[j].append(index)
-  for i in range(len(u)):
-    print("\r\n", u[i], ":")
-    counts = Counter(hap[i])
-    for j in range(1, len(u)+1):
-      perc = (counts[j]/len(hap[i]))*100
-      print(j, ":", perc)
+    if modo == 0:
+      for s in test:
+        print(s)
+      print()
+    else:
+      for j in range(len(x)):
+        index = test.index(x[j]) + 1
+        hap[j].append(index)
+  if modo == 1:
+    for i in range(len(x)):
+      print("\r\n", x[i], ":")
+      counts = Counter(hap[i])
+      for j in range(1, len(x)+1):
+        perc = (counts[j]/len(hap[i]))*100
+        print(j, ":", perc)
