5c5a0d36df880c8fbf058f957126efa69dec34e8
rafa-pdes90 <rafa-pdes90@users.noreply.github.com>
2018-11-07 21:14:37 -0300

    tests on my_shuffle only

--- shuffle_exp/shuffle_exp.py
+++ shuffle_exp/shuffle_exp.py
@@ -17,6 +17,7 @@ class Song:
     return "{0.name} {0.artist} {0.album} {0.weight}".format(self)
 
 if __name__=='__main__':
+  '''
   x = [Song(1, "A", "a1", 3), Song(2, "A", "a2", 7), Song(3, "B", "b1", 1), Song(4, "C", "c1", 2),
       Song(5, "B", "b1", 5), Song(6, "A", "a1", 9), Song(7, "D", "d1", 6), Song(8, "A", "a1", 4),
       Song(9, "D", "d2", 8)]
@@ -61,7 +62,7 @@ if __name__=='__main__':
     sum += v
   for i in range(1, 10):
     print (i, ": ", (hap[i]/sum)*100, "%")
-
+  '''
   u = [Song(1, "A", "a1", 9), Song(2, "B", "b1", 8), Song(3, "C", "c1", 7), Song(4, "D", "d1", 6),
       Song(5, "E", "e1", 5), Song(6, "F", "f1", 4), Song(7, "G", "g1", 3), Song(8, "H", "h1", 2),
       Song(9, "I", "i1", 1)]
